8941bb85e01e60eeb8ff99e40c3d6f3192823518
danush07 <danushhari2001@gmail.com>
2024-02-08 13:19:19 +0530

    Added New Banner and Slider Sections

---
+++ assets/custom-slider.js
@@ -0,0 +1,33 @@
+class SplideSlider extends HTMLElement {
+  constructor() {
+    super();
+
+    
+    console.log(this.element);
+    this.splideList = this.querySelectorAll(".splide__list");
+    console.log('from custom element',this.splideList);
+    this.initializeSlider();
+
+    console.log(this.splideList);
+  }
+
+  initializeSlider() {
+      document.addEventListener("DOMContentLoaded", function () {
+        this.element = this.querySelector(".splide");
+      var splide = new Splide(this.element, {
+        type: "slide",
+        perPage: 4,
+        gap: "24px",
+        pagination: false,
+        breakpoints: {
+          1200: { perPage: 2, gap: "1rem" },
+          640: { perPage:1, gap: 0 },
+        },
+      });
+      splide.mount();
+    });
+  }
+}
+
+customElements.define("splide-slider", SplideSlider);
+
